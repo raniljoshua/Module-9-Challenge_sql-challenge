365440b381a00a4fa717c953e333de4621903ed8
raniljoshua <144379028+raniljoshua@users.noreply.github.com>
2023-11-11 08:37:03 -0700

    Added value length for varchar columns

--- EmployeeSQL/sql-challenge_schema.sql
+++ EmployeeSQL/sql-challenge_schema.sql
@@ -9,32 +9,32 @@ DROP TABLE IF EXISTS salaries;
 -- After Creating the following tables, Import the data in the same order (because of dependencies)
 
 CREATE TABLE departments (
-	dept_no VARCHAR PRIMARY KEY,
-	dept_name VARCHAR NOT NULL
+	dept_no VARCHAR(4) PRIMARY KEY,
+	dept_name VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE titles(
-	title_id VARCHAR PRIMARY KEY,
-	title VARCHAR NOT NULL
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE employees (
 	emp_no INTEGER PRIMARY KEY,
-	emp_title_id VARCHAR NOT NULL REFERENCES titles(title_id),
-	birthdate VARCHAR NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
-	hire_date VARCHAR NOT NULL
+	emp_title_id VARCHAR(5) NOT NULL REFERENCES titles(title_id),
+	birthdate VARCHAR(45) NOT NULL,
+	first_name VARCHAR(45) NOT NULL,
+	last_name VARCHAR(45) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date VARCHAR(45) NOT NULL
 );
 
 CREATE TABLE dept_emp(
 	emp_no INTEGER NOT NULL REFERENCES employees(emp_no),
-	dept_no VARCHAR NOT NULL REFERENCES departments(dept_no)
+	dept_no VARCHAR(4) NOT NULL REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(
-	dept_no VARCHAR NOT NULL REFERENCES departments(dept_no),
+	dept_no VARCHAR(4) NOT NULL REFERENCES departments(dept_no),
 	emp_no INTEGER NOT NULL REFERENCES employees(emp_no)
 );
 
