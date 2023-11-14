2355a3be340dcf4562b0f0fb4f03e72d25041a39
raniljoshua <144379028+raniljoshua@users.noreply.github.com>
2023-11-14 12:03:59 -0700

    Updated Foreign Key

--- EmployeeSQL/sql-challenge_schema.sql
+++ EmployeeSQL/sql-challenge_schema.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS actor;
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
 DROP TABLE IF EXISTS employees;
 DROP TABLE IF EXISTS titles;
-DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS departments;
+
 
 -- After Creating the following tables, Import the data in the same order (because of dependencies)
 
@@ -20,27 +20,33 @@ CREATE TABLE titles(
 
 CREATE TABLE employees (
 	emp_no INTEGER PRIMARY KEY,
-	emp_title_id VARCHAR(5) NOT NULL REFERENCES titles(title_id),
+	emp_title_id VARCHAR(5) NOT NULL,
 	birthdate VARCHAR(45) NOT NULL,
 	first_name VARCHAR(45) NOT NULL,
 	last_name VARCHAR(45) NOT NULL,
 	sex VARCHAR(1) NOT NULL,
-	hire_date VARCHAR(45) NOT NULL
+	hire_date VARCHAR(45) NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
 
 CREATE TABLE dept_emp(
-	emp_no INTEGER NOT NULL REFERENCES employees(emp_no),
-	dept_no VARCHAR(4) NOT NULL REFERENCES departments(dept_no)
+	emp_no INTEGER NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 CREATE TABLE dept_manager(
-	dept_no VARCHAR(4) NOT NULL REFERENCES departments(dept_no),
-	emp_no INTEGER NOT NULL REFERENCES employees(emp_no)
+	dept_no VARCHAR(4) NOT NULL,
+	emp_no INTEGER NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 CREATE TABLE salaries(
-	emp_no INTEGER NOT NULL REFERENCES employees(emp_no),
-	salary INTEGER NOT NULL
+	emp_no INTEGER NOT NULL,
+	salary INTEGER NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 
